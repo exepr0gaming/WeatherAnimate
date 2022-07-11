e9c60e86071f5dd38c9649defaa3be7ac7d64667
AndrewKurdin <exehomework@gmail.com>
2022-07-11 14:35:38 +0300

    Initial Commit

---
+++ WeatherAnimate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherAnimate
+//
+//  Created by Курдин Андрей on 11.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherAnimate/WeatherAnimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAnimateApp.swift
+//  WeatherAnimate
+//
+//  Created by Курдин Андрей on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAnimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
